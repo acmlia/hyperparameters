df77fd3d47dda2299835085fd3a6573581ba7693
acmlia <lia.meteorologia@gmail.com>
2019-03-06 23:43:30 -0300

    tuning_activation_function.py corrigida

--- tuning_activation_function.py
+++ tuning_activation_function.py
@@ -31,55 +31,56 @@ class TuningActivationFunction:
     # Function to create model, required for KerasRegressor:
 
     def create_model(self, activation='relu'):
-    # create model
-    model.add(Dense(5, input_dim=9, kernel_initializer='uniform', activation=activation))
-    model.add(Dense(5, kernel_initializer='uniform', activation='activation'))
-    model.add(Dense(1, activation='linear'))
+        # create model
+        model = Sequential()
+        model.add(Dense(5, input_dim=9, kernel_initializer='uniform', activation=activation))
+        model.add(Dense(5, kernel_initializer='uniform', activation=activation))
+        model.add(Dense(1, activation='linear'))
 
     # Compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
 
     def run_TuningActivationFunction(self):
 
-    # Fix random seed for reproducibility:
-    seed = 7
-    np.random.seed(seed)
+        # Fix random seed for reproducibility:
+        seed = 7
+        np.random.seed(seed)
 
-    # Load dataset:
-    path = '/media/david/DATA/'
-    file = 'yearly_br_rain_var2d_OK.csv'
-    df = pd.read_csv(os.path.join(path, file), sep=',', decimal='.')
+        # Load dataset:
+        path = '/media/DATA/tmp/datasets/brazil/qgis/rain/'
+        file = 'yearly_br_rain_var2d_OK.csv'
+        df = pd.read_csv(os.path.join(path, file), sep=',', decimal='.')
 
-    # Split into input (X) and output (Y) variables:
-    df2 = df[['36V', '36H', '89V', '89H', '166V', '166H', '190V', 'PCT36', 'PCT89']]
-    # x = df2.reindex(columns=cols)
-    x = df2[['36V', '36H', '89V', '89H', '166V', '166H', '190V', 'PCT36', 'PCT89']]
-    y = df[['sfcprcp']]
+        # Split into input (X) and output (Y) variables:
+        df2 = df[['36V', '36H', '89V', '89H', '166V', '166H', '190V', 'PCT36', 'PCT89']]
+        # x = df2.reindex(columns=cols)
+        x = df2[['36V', '36H', '89V', '89H', '166V', '166H', '190V', 'PCT36', 'PCT89']]
+        y = df[['sfcprcp']]
 
-    # Scaling the input paramaters:
-    scaler = StandardScaler()
-    x_scaled = scaler.fit_transform(x)
+        # Scaling the input paramaters:
+        scaler = StandardScaler()
+        x_scaled = scaler.fit_transform(x)
 
-    # Split the dataset in test and train samples:
-    x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.25, random_state=101)
+        # Split the dataset in test and train samples:
+        x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.25, random_state=101)
 
-    # Create the instance for KerasRegressor:
-    model = KerasRegressor(build_fn=self.create_model, epochs=1000, batch_size=10, verbose=0)
+        # Create the instance for KerasRegressor:
+        model = KerasRegressor(build_fn=self.create_model, epochs=1000, batch_size=10, verbose=0)
 
-    # Define the grid search parameters
-    activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
-    param_grid = dict(activation=activation)
-    grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
-    grid_result = grid.fit(x_train, y_train)
+        # Define the grid search parameters
+        activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+        param_grid = dict(activation=activation)
+        grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1)
+        grid_result = grid.fit(x_train, y_train)
 
-    # summarize results
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-    means = grid_result.cv_results_['mean_test_score']
-    stds = grid_result.cv_results_['std_test_score']
-    params = grid_result.cv_results_['params']
-    for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+        # summarize results
+        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+        means = grid_result.cv_results_['mean_test_score']
+        stds = grid_result.cv_results_['std_test_score']
+        params = grid_result.cv_results_['params']
+        for mean, stdev, param in zip(means, stds, params):
+            print("%f (%f) with: %r" % (mean, stdev, param))
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
